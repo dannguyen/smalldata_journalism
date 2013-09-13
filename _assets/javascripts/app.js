94dd026ed9332c45da49904cb8f5728b16bfa7e6
dannguyen <dansonguyen@gmail.com>
2013-09-13 07:56:46 -0400

    less js

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,5 +1,27 @@
 //= require vendor/jquery/jquery
-//= require_tree ./vendor/bootstrap
+//= require vendor/bootstrap/transition
+//= require vendor/bootstrap/dropdown
+//= require vendor/bootstrap/collapse
 //= require_directory .
+
+
+$(document).ready(function(){
+
+    var $root = $('html, body');
+
+    $('a').click(function() {
+        var href = $.attr(this, 'href').replace(/^\//, '');
+
+        var href_offset = (href === '#') ? 0 : $(href).offset().top;
+        $root.animate({
+            scrollTop: href_offset
+        }, 500, function () {
+            if(href !== '#'){
+                window.location.hash = href;
+            }            
+        });
+        return false;
+    });
+})
 
 
